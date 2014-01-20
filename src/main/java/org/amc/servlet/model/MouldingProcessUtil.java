f50490016c36ad8fc7c8e239ca99610f53de3f67
subwoofer359 <subwoofer359@gmail.com>
2014-01-20 06:19:37 +0000

    Improved getMouldClosingTimeData method

--- src/main/java/org/amc/servlet/model/MouldingProcessUtil.java
+++ src/main/java/org/amc/servlet/model/MouldingProcessUtil.java
@@ -82,11 +82,6 @@ public class MouldingProcessUtil
 		return (finalVelocity-intialVelocity)/acceleration;
 	}
 	
-	/**
-	 * 
-	 * @param process
-	 * @return summation of Mould Closing Time
-	 */
 	public static float getMouldClosingTime(MouldingProcess process)
 	{
 		float[][] data=getMouldClosingTimeData(process);
@@ -104,7 +99,8 @@ public class MouldingProcessUtil
 	 * @param process
 	 * @return an array of floats [[time_1,distance_1],...]
 	 */
-	public static float[][] getMouldClosingTimeData(MouldingProcess process)
+	@Deprecated
+	public static float[][] getOldMouldClosingTimeData(MouldingProcess process)
 	{
 		float[][] data=new float[8][3];
 		float expectedDistance1=process.getMouldClosingOpenLimitPos()-process.getMouldClosedLimitPos();
@@ -203,6 +199,152 @@ public class MouldingProcessUtil
 		data[7][1]=process.getClsSPPos();
 		data[7][2]=(CLAMP_CLOSING_ACCELERATION*timeH);
 		
+		for(int i=0;i<data.length;i++)
+		{
+			System.out.println("("+data[i][1]+","+data[i][0]+")");
+		}
+		return data;
+}
+	
+
+	
+	/**
+	 * todo Fix the problem when acceleration/deceleration distance is greater than the pre-set distance
+	 * @param process
+	 * @return an array of floats [[time_1,distance_1],...]
+	 */
+	public static float[][] getMouldClosingTimeData(MouldingProcess process)
+	{
+		float[][] data=new float[8][3];
+		/*
+		 * Calculate the time taken from mould protect position to closed position
+		 * Initial velocity is zero
+		 * Distance: Mould protect position to zero = Mould protect position
+		 * t=sqrt(2*s/a)
+		 */
+		float closingTime=(float)Math.sqrt((2*process.getClsSPPos())/CLAMP_CLOSING_ACCELERATION);
+		data[data.length-1][0]=closingTime;
+		data[data.length-1][1]=process.getClsSPPos();
+		data[data.length-1][2]=(CLAMP_CLOSING_ACCELERATION*closingTime);
+		
+		/*
+		 * Calculate the time taken to reach mould protect position where mould velocity is zero
+		 * Initial velocity is CLS SP Speed
+		 * Final velocity is zero (At mould protect position)
+		 * Distance: Time taken to decelerate to zero velocity.
+		 * Time/Distance is inclusive of the time at mould cls sp pos
+		 */
+		data[data.length-2][0]=getTime(process.getClsSPSpeed(), 0,CLAMP_CLOSING_DEACCELERATION);
+		data[data.length-2][1]=getDistance(process.getClsSPSpeed(), data[data.length-2][0], CLAMP_CLOSING_DEACCELERATION);
+		data[data.length-2][2]=(CLAMP_CLOSING_DEACCELERATION*data[data.length-2][0]);
+	
+		//Distance at each set mould clsoing speed
+		float[] posData={
+				process.getMouldClosingOpenLimitPos()-process.getMouldClosedLimitPos(),
+				process.getMouldClosedLimitPos()-process.getClsSlowPos(),
+				process.getClsSlowPos()-process.getClsSPPos()
+		};
+		
+		//Defined mould closing speed
+		float[] speedData={
+				process.getMouldClosingOpenLimitSpeed(),
+				process.getMouldClosedLimitSpeed(),
+				process.getClsSPSpeed()
+		};
+		
+		int i=0;// Pointer for posData and speedData array
+		int t=0;// Pointer for the result array: Data[][]
+		float time=0f;
+		float distance=0f;
+		float speed=0f;
+		//Calculate the areas of velocity change
+		while(i<posData.length)
+		{
+			//Check if there's an acceleration or deceleration
+			if(MouldingProcessUtil.getAcceleration(speed, speedData[i],2)>0)
+			{
+				//Calculate transitions in the graph
+				time=getTime(speed,speedData[i],CLAMP_CLOSING_ACCELERATION);
+				distance=getDistance(speed, time, CLAMP_CLOSING_ACCELERATION);
+				speed=speedData[i];
+				data[t][0]=time;
+				data[t][1]=distance;
+				data[t][2]=speed;
+			}
+			else
+			{
+				time=getTime(speed,speedData[i],CLAMP_CLOSING_DEACCELERATION);
+				distance=getDistance(speed, time, CLAMP_CLOSING_DEACCELERATION);
+				speed=speedData[i];
+				data[t][0]=time;
+				data[t][1]=distance;
+				data[t][2]=speed;
+			}
+			i++;
+			t=t+2;//Store result in every odd index in the result array so we can calculate the even index results later
+			
+			
+		}
+		//Reset Counters and temporary variables
+		i=0;t=1;//T=1 so we can work on the even index results in the results array (data[][])
+		distance=0;speed=0;time=0;
+		while(i<posData.length)
+		{
+			distance=posData[i];
+			speed=speedData[i];
+			float distanceDelta=0f;
+			/*
+			 * The initial mould acceleration distance is included in the distance ClosingOpenLimitPos->ClosedLimitPos
+			 * Between the initial acceleration and final deceleration the time/distance is halved between each 
+			 * set distance
+			 */
+			if(i==0)
+			{
+				distanceDelta=data[t-1][1]+data[t+1][1]/2;
+			}
+			else
+			/*
+			 * The final deceleration to zero velocity at mould protect position
+			 */
+			if(i==posData.length-1)
+			{
+				distanceDelta=data[t-1][1]/2+data[t+1][1];
+			
+			}
+			else
+			{
+				distanceDelta=data[t-1][1]/2+data[t+1][1]/2;
+			}
+			
+			//If the distance change is greater than the distance of pre set position then distance is set to zero
+			if(distanceDelta>distance)
+			{
+				distance=0;
+			}
+			else
+			{
+				distance-=distanceDelta;
+			}
+			//Calculate time and save in result array
+			time=distance/speed;
+			data[t][0]=time;
+			data[t][1]=distance;
+			data[t][2]=speed;
+			i++;
+			t=t+2;
+		}
+		
+		distance=0f;
+		time=0f;
+		for(float[] f:data)
+		{
+			distance+=f[1];
+			time+=f[0];
+			System.out.printf("Distance(%.3f) Time(%.3f) Speed(%.3f)%n",f[1],f[0],f[2]);
+			//System.out.printf("Distance(%.3f) Time(%.3f) Speed(%.3f)%n",distance,time,f[2]);
+		}
+		
+		
 		return data;
 	}
 	
@@ -211,16 +353,17 @@ public class MouldingProcessUtil
 	{
 		
 		MouldingProcess p=new MouldingProcess();
-		p.setMouldClosingOpenLimitPos(320);
+		p.setMouldClosingOpenLimitPos(388);
 		p.setMouldClosedLimitPos(250);
-		p.setClsSlowPos(160);
-		p.setClsSPPos(62);
-		p.setMouldClosingOpenLimitSpeed(350);
-		p.setMouldClosedLimitSpeed(350);
+		p.setClsSlowPos(190);
+		p.setClsSPPos(68.9f);
+		p.setMouldClosingOpenLimitSpeed(415);
+		p.setMouldClosedLimitSpeed(250);
 		p.setClsSPSpeed(200);
 		
-		System.out.println(getMouldClosingTime(p));
-		
+		//System.out.println(getMouldClosingTime(p));
+		getMouldClosingTimeData(p);
+		getOldMouldClosingTimeData(p);
 		
 		
 		
