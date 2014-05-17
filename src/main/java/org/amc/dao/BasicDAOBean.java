5aff3a0caf09dbb7cd87cd17d0fbf960a38deb18
Adrian McLaughlin <adrian@adrianmclaughlin.ie>
2014-05-17 20:10:32 +0100

    Added some java doc

--- src/main/java/org/amc/dao/BasicDAOBean.java
+++ src/main/java/org/amc/dao/BasicDAOBean.java
@@ -8,27 +8,42 @@ import java.sql.Statement;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
-
+/**
+ * A base class for other Data Access Objects
+ * @author Adrian McLaughlin
+ * @version $REV$
+ */
 public class BasicDAOBean implements DAOBeanRemote, Serializable
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 2637719986763223271L;
 	
 	@Resource(name="MyDataSource")
 	private DataSource dataSource;
 
-	
+	/**
+	 * Non argument Constructor
+	 */
 	public BasicDAOBean()
 	{
-		
+		;;
 	}
 
+	/**
+	 * @return java.sql.Connection
+	 * @throws SQLException if this data source is null
+	 */
 	public Connection getConnection() throws SQLException
 	{
 		return dataSource.getConnection();		
 	}
+	
+	/**
+	 * Closes java.sql objects after transaction is finished.
+	 * Must be explicitly called by the user.
+	 * @param resultSet
+	 * @param statement
+	 * @param connection
+	 */
 	protected void closeDBObjects(ResultSet resultSet,Statement statement,Connection connection)
 	{
 		if(resultSet!=null)
