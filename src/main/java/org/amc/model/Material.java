46c413f2214b43a95764b23248f1ae0578c30905
Adrian McLaughlin <adrian@adrianmclaughlin.ie>
2014-05-20 20:09:37 +0100

    Changed Material from a Stateful bean to a JPA Entity

--- src/main/java/org/amc/model/Material.java
+++ src/main/java/org/amc/model/Material.java
@@ -2,283 +2,204 @@ package org.amc.model;
 
 import java.io.Serializable;
 
-import javax.ejb.Stateful;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * POJO to represent a material used for plastic injection moulding
  * @author adrian
  *
  */
-@Stateful
-public class Material implements MaterialRemote, Serializable
+@Entity
+@Table(name="material")
+public class Material implements Serializable
 {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 2611230247508857580L;
+	
+	@Id
 	private int id;
+	/**
+	 *  The name of the company that produces the material
+	 */
+	@Column(nullable=false)
 	private String company;
+	/**
+	 * The name of the material itself e.g. Moplen 540p
+	 */
+	@Column(nullable=false)
 	private String name;
+	
+	/**
+	 * The type of material e.g. PVC/ABS/PP/PE
+	 */
+	@Column(nullable=false)
 	private String type;
+	
+	@Column
 	private float density;
+	@Column
 	private float linear_expansion;
+	@Column
 	private float water_absorption;
-	private float material_drying;	  
+	@Column
+	private float material_drying;
+	@Column
 	private float melting_temp_lower;
+	@Column
 	private float melting_temp_upper;
+	@Column
 	private float mould_shrinkage;
+	@Column
 	private float mould_temp_low;
+	@Column
 	private float mould_temp_upper;
 
 	public Material()
 	{
-		
+		;;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getId()
-	 */
-	@Override
 	public int getId()
 	{
 		return id;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getCompany()
-	 */
-	@Override
 	public String getCompany()
 	{
 		return company;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getName()
-	 */
-	@Override
 	public String getName()
 	{
 		return name;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getType()
-	 */
-	@Override
 	public String getType()
 	{
 		return type;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getDensity()
-	 */
-	@Override
 	public float getDensity()
 	{
 		return density;
 	}
-
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getLinear_expansion()
-	 */
-	@Override
+	
 	public float getLinear_expansion()
 	{
 		return linear_expansion;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getWater_absorption()
-	 */
-	@Override
 	public float getWater_absorption()
 	{
 		return water_absorption;
 	}
-
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getMaterial_drying()
-	 */
-	@Override
+	
 	public float getMaterial_drying()
 	{
 		return material_drying;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getMelting_temp_lower()
-	 */
-	@Override
 	public float getMelting_temp_lower()
 	{
 		return melting_temp_lower;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getMelting_temp_upper()
-	 */
-	@Override
 	public float getMelting_temp_upper()
 	{
 		return melting_temp_upper;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getMould_shrinkage()
-	 */
-	@Override
 	public float getMould_shrinkage()
 	{
 		return mould_shrinkage;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getMould_temp_low()
-	 */
-	@Override
 	public float getMould_temp_low()
 	{
 		return mould_temp_low;
 	}
-
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#getMould_temp_upper()
-	 */
-	@Override
+	
 	public float getMould_temp_upper()
 	{
 		return mould_temp_upper;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setId(int)
-	 */
-	@Override
 	public void setId(int id)
 	{
 		this.id = id;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setCompany(java.lang.String)
-	 */
-	@Override
 	public void setCompany(String company)
 	{
 		this.company = company;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setName(java.lang.String)
-	 */
-	@Override
 	public void setName(String name)
 	{
 		this.name = name;
 	}
-
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setType(java.lang.String)
-	 */
-	@Override
+	
 	public void setType(String type)
 	{
 		this.type = type;
 	}
-
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setDensity(float)
-	 */
-	@Override
+	
 	public void setDensity(float density)
 	{
 		this.density = density;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setLinear_expansion(float)
-	 */
-	@Override
 	public void setLinear_expansion(float linear_expansion)
 	{
 		this.linear_expansion = linear_expansion;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setWater_absorption(float)
-	 */
-	@Override
 	public void setWater_absorption(float water_absorption)
 	{
 		this.water_absorption = water_absorption;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setMaterial_drying(float)
-	 */
-	@Override
 	public void setMaterial_drying(float material_drying)
 	{
 		this.material_drying = material_drying;
 	}
-
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setMelting_temp_lower(float)
-	 */
-	@Override
+	
 	public void setMelting_temp_lower(float melting_temp_lower)
 	{
 		this.melting_temp_lower = melting_temp_lower;
 	}
-
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setMelting_temp_upper(float)
-	 */
-	@Override
+	
 	public void setMelting_temp_upper(float melting_temp_upper)
 	{
 		this.melting_temp_upper = melting_temp_upper;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setMould_shrinkage(float)
-	 */
-	@Override
 	public void setMould_shrinkage(float mould_shrinkage)
 	{
 		this.mould_shrinkage = mould_shrinkage;
 	}
-
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setMould_temp_low(float)
-	 */
-	@Override
+	
 	public void setMould_temp_low(float mould_temp_low)
 	{
 		this.mould_temp_low = mould_temp_low;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.model.MaterialRemote#setMould_temp_upper(float)
-	 */
-	@Override
 	public void setMould_temp_upper(float mould_temp_upper)
 	{
 		this.mould_temp_upper = mould_temp_upper;
 	}
 
-	@Override
 	public String toString()
 	{
 		return this.getCompany()+" "+this.getName()+" "+this.getType();
 	}
 	
-	@Override
 	public boolean equals(Object material)
 	{
-		MaterialRemote m2=(MaterialRemote)material;
+		Material m2=(Material)material;
 		
 		if(m2 instanceof Material)
 		{
