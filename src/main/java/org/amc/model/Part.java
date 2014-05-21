b91c6c9cb4c0fe9887e8a9d839885fdbbac3e597
Adrian McLaughlin <adrian@adrianmclaughlin.ie>
2014-05-21 16:35:19 +0100

    Changed Part into an entity

--- src/main/java/org/amc/model/Part.java
+++ src/main/java/org/amc/model/Part.java
@@ -1,60 +1,68 @@
 package org.amc.model;
 
-
 import java.io.Serializable;
 
-import javax.ejb.Local;
-import javax.ejb.Stateful;
-
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * <p>
  * Project: APL_Problem_Database
  * </p>
  * <p>
- * file: $URL: file:///home/adrian/Documents/SVNRepository/Java/APL_Problem_Database/trunk/APL_Problem_Database/src/apl_problem_database/JobTemplate.java $
+ * file: $URL:
+ * file:///home/adrian/Documents/SVNRepository/Java/APL_Problem_Database
+ * /trunk/APL_Problem_Database/src/apl_problem_database/JobTemplate.java $
  * <p>
  * <p>
  * Created on May 4, 2006
  * </p>
  * 
  * @author adrian
- * @version $Revision: 888 $
- * Represents a job running in the factory
+ * @version $Revision: 888 $ Represents a job running in the factory
  */
-@Stateful
-public class Part implements PartRemote, PartLocal,Serializable
+@Entity
+@Table(name = "jobtemplate")
+public class Part implements Serializable
 {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = -8498315270583017514L;
+
+	@Id
 	private int id;
+	@Column
 	private String colour;
+	@Column(nullable = false)
 	private String company;
+	@Column(nullable = false)
 	private boolean external;
+	@Column(nullable = false)
 	private String name;
+	@Column(nullable = false)
 	private String part_id;
+	@Column(nullable = false)
 	private String qss_no;
+	@Column
 	private String revision;
+	@Column
 	private String version;
-	 
-	
-	
+
 	/**
 	 * 
 	 * Constructor for JobTemplate.java
 	 */
 	public Part()
 	{
-		
-	
+		;
+		;
 	}
-	
+
 	/**
 	 * 
 	 * Constructor for JobTemplate.java
+	 * 
 	 * @param name
 	 * @param part_id
 	 * @param company
@@ -63,165 +71,124 @@ public class Part implements PartRemote, PartLocal,Serializable
 	 * @param colour
 	 * @param external
 	 */
-	public Part(String name,
-						String part_id,
-						String company,
-						String version,
-						String revision,
-						String colour,
-						boolean external,
-						String qss_no
-					)
+	public Part(
+			String name, 
+			String part_id, 
+			String company, 
+			String version,
+			String revision, 
+			String colour, 
+			boolean external, 
+			String qss_no)
 	{
-			
-			this.name=name;
-			this.part_id=part_id;
-			this.company=company;
-			this.version=version;
-			this.revision=revision;
-			this.colour=colour;
-			this.external=external;
-			this.qss_no=qss_no;
-			
-			
+
+		this.name = name;
+		this.part_id = part_id;
+		this.company = company;
+		this.version = version;
+		this.revision = revision;
+		this.colour = colour;
+		this.external = external;
+		this.qss_no = qss_no;
+
 	}
 
-	@Override
 	public String toString()
 	{
-		String value= String.valueOf(getName())+" ";
-		value=value+String.valueOf(getVersion())+" ";
-		value=value+String.valueOf(getRevision())+" ";
-		value=value+String.valueOf(getColour());
+		String value = String.valueOf(getName()) + " ";
+		value = value + String.valueOf(getVersion()) + " ";
+		value = value + String.valueOf(getRevision()) + " ";
+		value = value + String.valueOf(getColour());
 		return value;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#getColour()
-	 */
-	@Override
-	public String getColour() {
+	public String getColour()
+	{
 		return colour;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#setColour(java.lang.String)
-	 */
-	@Override
-	public void setColour(String colour) {
+
+	public void setColour(String colour)
+	{
 		this.colour = colour;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#getCompany()
-	 */
-	@Override
-	public String getCompany() {
+
+	public String getCompany()
+	{
 		return company;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#setCompany(java.lang.String)
-	 */
-	@Override
-	public void setCompany(String company) {
+
+	public void setCompany(String company)
+	{
 		this.company = company;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#getExternal()
-	 */
-	@Override
-	public boolean getExternal() {
+
+	public boolean getExternal()
+	{
 		return external;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#setExternal(boolean)
-	 */
-	@Override
-	public void setExternal(boolean external) {
+
+	public void setExternal(boolean external)
+	{
 		this.external = external;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#getName()
-	 */
-	@Override
-	public String getName() {
+
+	public String getName()
+	{
 		return name;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#setName(java.lang.String)
-	 */
-	@Override
-	public void setName(String name) {
+
+	public void setName(String name)
+	{
 		this.name = name;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#getPart_id()
-	 */
-	@Override
-	public String getPart_id() {
+
+	public String getPart_id()
+	{
 		return part_id;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#setPart_id(java.lang.String)
-	 */
-	@Override
-	public void setPart_id(String part_id) {
+
+	public void setPart_id(String part_id)
+	{
 		this.part_id = part_id;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#getQss_no()
-	 */
-	@Override
-	public String getQss_no() {
+
+	public String getQss_no()
+	{
 		return qss_no;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#setQss_no(java.lang.String)
-	 */
-	@Override
-	public void setQss_no(String qss_no) {
+
+	public void setQss_no(String qss_no)
+	{
 		this.qss_no = qss_no;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#getRevision()
-	 */
-	@Override
-	public String getRevision() {
+
+	public String getRevision()
+	{
 		return revision;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#setRevision(java.lang.String)
-	 */
-	@Override
-	public void setRevision(String revision) {
+
+	public void setRevision(String revision)
+	{
 		this.revision = revision;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#getVersion()
-	 */
-	@Override
-	public String getVersion() {
+
+	public String getVersion()
+	{
 		return version;
 	}
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#setVersion(java.lang.String)
-	 */
-	@Override
-	public void setVersion(String version) {
+
+	public void setVersion(String version)
+	{
 		this.version = version;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#getId()
-	 */
-	@Override
-	public int getId() {
+	public int getId()
+	{
 		return id;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.amc.servlet.model.PartRemote#setId(int)
-	 */
-	@Override
-	public void setId(int id) {
+	public void setId(int id)
+	{
 		this.id = id;
 	}
 
