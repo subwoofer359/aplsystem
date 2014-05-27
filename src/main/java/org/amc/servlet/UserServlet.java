938e762c051469606218d019bd741f0c0bf84b2f
Adrian McLaughlin <adrian@adrianmclaughlin.ie>
2014-05-27 16:23:55 +0100

    Users.jsp now list users and they are selectable

--- src/main/java/org/amc/servlet/UserServlet.java
+++ src/main/java/org/amc/servlet/UserServlet.java
@@ -1,10 +1,14 @@
 package org.amc.servlet;
 
+import java.util.List;
+
+import org.amc.dao.UserDAO;
+import org.amc.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.apache.log4j.Logger;
 /**
  * 
  * @author Adrian Mclaughlin
@@ -13,6 +17,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserServlet
 {
+	private static UserDAO userDAO;
+	private static Logger logger=Logger.getLogger(UserServlet.class);
+	
 	@RequestMapping("/User")
 	public String getUserPage()
 	{
@@ -20,10 +27,22 @@ public class UserServlet
 	}
 	
 	@RequestMapping("/Users")
-	public ModelAndView getUsersPage(Model model)
+	public ModelAndView getUsersPage(ModelAndView model)
 	{
-		ModelAndView mv=new ModelAndView();
-		mv.setViewName("Users");
-		return mv;
+		model.setViewName("Users");
+		System.out.println("DAO="+userDAO);
+		List<User> list=userDAO.findUsers();
+		model.getModel().put("users", list);
+		
+		return model;
+	}
+	
+	/*
+	 * Spring injected
+	 */
+	public static void setUserDAO(UserDAO ud)
+	{
+		userDAO=ud;
+		logger.debug("UserDAO added to UserServlet:"+userDAO);
 	}
 }
--- src/main/webapp/WEB-INF/JSP/Users.jsp
+++ src/main/webapp/WEB-INF/JSP/Users.jsp
@@ -10,6 +10,7 @@
 <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/General.css">
 <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/SearchPage.css">
 <title>Users</title>
+<script src="${pageContext.request.contextPath}/js/SearchPage.js"></script>
 </head>
 <body>
 <DIV class="title"><H1>Users</H1></DIV>
@@ -19,11 +20,11 @@
 <div class="results">
 <table>
 <thead>
-<tr><th>UserName</th><th>Full Name</th><th>Email</th><th>Active</th></tr>
+<tr><th>UserName</th><th>Full Name</th><th>Email</th><th>Active</th><th></th></tr>
 </thead>
 <tbody>
 <c:forEach var="user" items="${users}">
-<tr><td><c:out value="${user.userName}" /></td><td><c:out value="${user.fullName}"/></td><td><c:out value="${user.emailAddress}"/></td><td><c:out value="${user.active}"/></td></tr>
+<tr  onclick="selected(this)"><td><c:out value="${user.userName}" /></td><td><c:out value="${user.fullName}"/></td><td><c:out value="${user.emailAddress}"/></td><td><c:out value="${user.active}"/></td><td  class="checkbox"><input type="checkbox" name="edit" value="${material.value.id}"/></td></tr>
 </c:forEach>
 </tbody>
 </table>
