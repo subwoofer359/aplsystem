246c117250d54daef16166648a96fc221a485002
Adrian McLaughlin <adrian@adrianmclaughlin.ie>
2014-06-02 19:27:35 +0100

    Created SPC entities

---
+++ src/main/java/org/amc/model/spc/SPCData.java
@@ -0,0 +1,142 @@
+package org.amc.model.spc;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+
+import org.amc.model.User;
+
+/**
+ * @author Adrian McLaughlin
+ * @version 1
+ * Represents saved SPC data
+ */
+@Entity
+//@Table not sure what to put for this value
+public class SPCData implements Serializable
+{
+	private static final long serialVersionUID = 5840756393419920613L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	/**
+	 * Required by JPA for concurrency
+	 */
+	@Version
+	private long version;
+	
+	/**
+	 * The date the measurement was taken
+	 */
+	@NotNull
+	@Column
+	private Date date;
+	
+	/**
+	 * The user that took the measurement
+	 */
+	@NotNull
+	@ManyToOne
+	private User user;
+	
+	/**
+	 * The number of this measurement e.g. 3 of 5
+	 */
+	@NotNull
+	@Column
+	private int measurementNumber;
+	
+	/**
+	 * The actual measurement
+	 */
+	@NotNull
+	@Column
+	private float measurement;
+	
+	/**
+	 * The SPCMeasure object this SPCData is reference by
+	 */
+	@NotNull
+	@ManyToOne
+	@JoinColumn
+	private SPCMeasurement spcMeasurement;
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public long getVersion()
+	{
+		return version;
+	}
+
+	public Date getDate()
+	{
+		return date;
+	}
+
+	public User getUser()
+	{
+		return user;
+	}
+
+	public int getMeasurementNumber()
+	{
+		return measurementNumber;
+	}
+
+	public float getMeasurement()
+	{
+		return measurement;
+	}
+
+	public SPCMeasurement getSpcMeasurement()
+	{
+		return spcMeasurement;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public void setDate(Date date)
+	{
+		this.date = date;
+	}
+
+	public void setUser(User user)
+	{
+		this.user = user;
+	}
+
+	public void setMeasurementNumber(int measurementNumber)
+	{
+		this.measurementNumber = measurementNumber;
+	}
+
+	public void setMeasurement(float measurement)
+	{
+		this.measurement = measurement;
+	}
+
+	public void setSpcMeasurement(SPCMeasurement spcMeasurement)
+	{
+		this.spcMeasurement = spcMeasurement;
+	}
+	
+	
+
+}
---
+++ src/main/java/org/amc/model/spc/SPCMeasurement.java
@@ -0,0 +1,182 @@
+package org.amc.model.spc;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+
+import org.amc.model.Part;
+
+
+/**
+ * 
+ * @author Adrian McLaughlin
+ * @version 1
+ *
+ * JPA Entity to store information of what SPC Measurements should be stored
+ */
+@Entity
+@Table(name="SPCMeasurements")
+public class SPCMeasurement implements Serializable
+{
+
+	private static final long serialVersionUID = 5160093236226158094L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	/**
+	 * The part the SPC measurements will be taken from
+	 */
+	@ManyToOne
+	@JoinColumn(name="part_id")
+	private Part part;
+	
+	
+	/**
+	 * The dimension of the part to be measured
+	 */
+	@NotNull
+	private String dimension;
+	
+	/**
+	 * The desired value of the measurement taken from the part
+	 */
+	@NotNull
+	@Column
+	private float nominal;
+	
+	/**
+	 * The upper tolerance 
+	 */
+	@NotNull
+	@Column
+	private float upperLimit;
+	
+	/**
+	 * The lower tolerance 
+	 */
+	@NotNull
+	@Column
+	private float lowerLimit;
+	
+	/**
+	 * The number of parts that have to be measured 
+	 */
+	@NotNull
+	@Column
+	private int noOfMeasurements;
+	
+	
+	/**
+	 * Is the SPC data still needed to be taken?
+	 */
+	@NotNull
+	@Column
+	private boolean active;
+
+	/**
+	 * Required by JPA for concurrency
+	 */
+	@Version
+	private long version;
+	//@Todo add reference to SPCData
+	
+	public int getId()
+	{
+		return id;
+	}
+
+	public Part getPart()
+	{
+		return part;
+	}
+
+	public String getDimension()
+	{
+		return dimension;
+	}
+
+	public float getNominal()
+	{
+		return nominal;
+	}
+
+	public float getUpperLimit()
+	{
+		return upperLimit;
+	}
+
+	public float getLowerLimit()
+	{
+		return lowerLimit;
+	}
+
+	public int getNoOfMeasurements()
+	{
+		return noOfMeasurements;
+	}
+
+	public boolean isActive()
+	{
+		return active;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public void setPart(Part part)
+	{
+		this.part = part;
+	}
+
+	public void setDimension(String dimension)
+	{
+		this.dimension = dimension;
+	}
+
+	public void setNominal(float nominal)
+	{
+		this.nominal = nominal;
+	}
+
+	public void setUpperLimit(float upperLimit)
+	{
+		this.upperLimit = upperLimit;
+	}
+
+	public void setLowerLimit(float lowerLimit)
+	{
+		this.lowerLimit = lowerLimit;
+	}
+
+	public void setNoOfMeasurements(int noOfMeasurements)
+	{
+		this.noOfMeasurements = noOfMeasurements;
+	}
+
+	public void setActive(boolean active)
+	{
+		this.active = active;
+	}
+
+	public long getVersion()
+	{
+		return version;
+	}
+	
+	
+	
+	
+}
