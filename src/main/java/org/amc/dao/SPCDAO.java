f71282b0b6e76b173c9c5a795ce12fed7e7a68aa
Adrian McLaughlin <adrian@adrianmclaughlin.ie>
2014-06-03 10:29:28 +0100

    SPCDAO.java:implmented methods update,delete,find and get

--- src/main/java/org/amc/dao/SPCDAO.java
+++ src/main/java/org/amc/dao/SPCDAO.java
@@ -1,11 +1,15 @@
 package org.amc.dao;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-
-import org.amc.model.Part;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
 import org.amc.model.spc.SPCMeasurement;
 
 public class SPCDAO extends DAO implements Serializable
@@ -27,4 +31,66 @@ public class SPCDAO extends DAO implements Serializable
 		em.close();
 	}
 
+	public void updateSPCMeasurement(SPCMeasurement measurement)
+	{
+		EntityManager em=getEntityManager();
+		em.getTransaction().begin();
+		em.merge(measurement);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	/**
+	 * 
+	 * @param measurementId
+	 * @return SPCMeasurement or null if no measurement with id is found
+	 */
+	public SPCMeasurement getSPCMeasurement(String measurementId)
+	{
+		EntityManager em=getEntityManager();
+		SPCMeasurement measurement=null;
+		Query q=em.createQuery("Select x from SPCMeasurement x where x.id="+measurementId+"");
+		try
+		{
+			measurement = (SPCMeasurement)q.getSingleResult();
+		}
+		catch(NoResultException nre)
+		{
+			//do nothing
+		}
+		em.close();
+
+		return measurement;
+	}
+
+	public List<SPCMeasurement> findSPCMeasurements(String col, String value)
+	{
+		EntityManager em=getEntityManager();
+		Query q=em.createQuery("Select x from SPCMeasurement x where x."+col+"='"+value+"'");
+		Map<Integer, SPCMeasurement> list = new HashMap<Integer, SPCMeasurement>();
+		List<SPCMeasurement> resultList=(List<SPCMeasurement>)q.getResultList();
+		em.close();
+		return resultList;
+	}
+
+	public List<SPCMeasurement> findSPCMeasurements()
+	{
+		EntityManager em=getEntityManager();
+		Query q=em.createQuery("Select x from SPCMeasurement x ORDER BY x.id");
+		Map<Integer, SPCMeasurement> list = new TreeMap<Integer, SPCMeasurement>();
+		List<SPCMeasurement> resultList=(List<SPCMeasurement>)q.getResultList();
+		em.close();
+		return resultList;
+	}
+
+	/** @todo needs to fail if there are any SPCData entities referencing this SPCMeasurement */
+	public void deleteSPCMeasurement(SPCMeasurement measurement)
+	{
+		EntityManager em=getEntityManager();
+		em.getTransaction().begin();
+		SPCMeasurement u=em.merge(measurement);
+		em.remove(u);
+		em.getTransaction().commit();
+		em.close();
+	}
 }
