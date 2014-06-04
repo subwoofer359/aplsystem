c40e995d3437f12b0f9c26b3c2ebc9c4aa8f210e
Adrian McLaughlin <adrian@adrianmclaughlin.ie>
2014-06-04 09:04:31 +0100

    SPC: Created blank files for my implementation of the SPC part of my project

---
+++ src/main/java/org/amc/servlet/APLSpcController.java
@@ -0,0 +1,16 @@
+package org.amc.servlet;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+
+@Controller
+/**
+ * 
+ * @author Adrian McLaughlin
+ * Controller for the SPC model and view
+ *
+ */
+public class APLSpcController
+{
+	private static Logger logger=Logger.getLogger(APLSpcController.class);
+}
---
+++ src/test/java/org/amc/myservlet/test/spc/TestAPLSpcController.java
@@ -0,0 +1,28 @@
+package org.amc.myservlet.test.spc;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAPLSpcController
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
