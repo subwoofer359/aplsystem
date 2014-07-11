5e5090cf4ba2504203655ede35d609d682c81e10
subwoofer359 <subwoofer359@gmail.com>
2014-07-11 06:31:09 +0100

    MyFunction.java:Commented code

--- src/main/java/org/amc/servlet/el/MyFunctions.java
+++ src/main/java/org/amc/servlet/el/MyFunctions.java
@@ -1,35 +1,46 @@
 package org.amc.servlet.el;
 
-/**
- * 
- * @author Adrian Mclaughlin
- * @version 1
- */
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.amc.model.Material;
 
+/**
+ * 
+ * @author Adrian Mclaughlin
+ * @version 1
+ */
 public final class MyFunctions
 {
 
-	private static Logger logger=Logger.getLogger(MyFunctions.class);
+	private static final Logger LOG=Logger.getLogger(MyFunctions.class);
 	
 	/**
 	 * Utility class
 	 */
 	private MyFunctions()
 	{
+		throw new AssertionError(getClass().getSimpleName()+" is a utility class");
 	}
 	
+	/**
+	 * @param request The HttpServletRequest from the user
+	 * @param role The role the user should be in
+	 * @return true if the user is in the role
+	 */
 	public static boolean isUserInRole(HttpServletRequest request,String role)
 	{
 		return request.isUserInRole(role);
 	}
 	
+	/**
+	 * To get a string representation of the Material
+	 * @param material Material Entity
+	 * @return String
+	 */
 	public static String toString(Material material)
 	{
-		 logger.info("Helper Function:"+material.getName());
+		 LOG.info("Helper Function:"+material.getName());
 		 return material.getCompany()+" "+material.getName()+" "+material.getType();
 	}
 }
