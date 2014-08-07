faf1381a65f1d1c33fc5cd2f2416c08a0ae11db9
Adrian McLaughlin <subwoofer359@gmail.com>
2014-08-07 19:41:33 +0100

    Added simple PartSearchBinder

--- src/main/java/org/amc/servlet/validator/PartSearchFormValidator.java
+++ src/main/java/org/amc/servlet/validator/PartSearchFormValidator.java
@@ -1,15 +1,38 @@
 package org.amc.servlet.validator;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.amc.servlet.action.search.PartSearch;
 import org.amc.servlet.model.PartSearchForm;
 
-public class PartSearchFormValidator
+public class PartSearchFormValidator extends WebPageFormValidator
 {
-	List<String> errors=new ArrayList<String>();
+	private static final SimpleDateFormat SDF=(SimpleDateFormat)SimpleDateFormat.getDateInstance();
+	
 	public List<String> validate(PartSearchForm form)
-	{	
+	{
+		List<String> errors=getErrors();
 		return errors;
+	}
+	
+	public static class PartSearchBinder
+	{
+		public static PartSearch getPartSearch(PartSearchForm form) throws ParseException
+		{
+			PartSearch partSearch=new PartSearch();
+			
+			partSearch.setCompany(form.getCompany());
+			partSearch.setPart(form.getPart());
+			partSearch.setQSSNumber(form.getQSSNumber());
+			
+			
+			partSearch.setStartDate(SDF.parse(form.getStartDate()));
+			partSearch.setEndDate(SDF.parse(form.getEndDate()));
+		
+			return partSearch;
+		}
 	}
 }
