0b7ff254448aea2f0deaebcf69617649545e4394
Adrian McLaughlin <subwoofer359@gmail.com>
2014-08-08 15:27:09 +0100

    Added Validator and Form classes for the MaterialSearch and MouldingProcessSearch classes

---
+++ src/main/java/org/amc/servlet/model/MaterialSearchForm.java
@@ -0,0 +1,53 @@
+package org.amc.servlet.model;
+
+
+public class MaterialSearchForm implements WebPageForm
+{
+	private String type;
+	private String name;
+	private String company;
+	/**
+	 * @return the type
+	 */
+	public String getType()
+	{
+		return type;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+	/**
+	 * @return the company
+	 */
+	public String getCompany()
+	{
+		return company;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	/**
+	 * @param company the company to set
+	 */
+	public void setCompany(String company)
+	{
+		this.company = company;
+	}
+	
+	
+}
