639bda05d2e38872b6ad2b0ff0a8f805546e2e1d
Adrian McLaughlin <subwoofer359@gmail.com>
2014-08-09 18:15:28 +0100

    Add findMaterials(Search), Refactored other methods to use the super class methods for accessing the database

--- src/main/java/org/amc/dao/MaterialDAO.java
+++ src/main/java/org/amc/dao/MaterialDAO.java
@@ -1,7 +1,7 @@
 package org.amc.dao;
 
 import java.io.Serializable;
-import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -9,22 +9,15 @@ import java.util.TreeMap;
 import org.amc.DAOException;
 import org.amc.model.Material;
 import org.amc.servlet.action.search.WebFormSearch;
-import org.apache.log4j.Logger;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceException;
-import javax.persistence.Query;
-
 /**
- * 
+ * Material Data Acess Object 
  * @author Adrian Mclaughlin
  * @version 1
  */
 public class MaterialDAO extends DAO<Material> implements Serializable
 {
 	private static final long serialVersionUID = -4397260307883862647L;
-	private static Logger logger=Logger.getLogger(MaterialDAO.class);
-	
+
 	public MaterialDAO()
 	{
 		super(Material.class);
@@ -33,46 +26,22 @@ public class MaterialDAO extends DAO<Material> implements Serializable
 
 	public Map<Integer, Material> findMaterials(String col, String value) throws DAOException
 	{
-		EntityManager em=getEntityManager();
-		Map<Integer, Material> list;
-		try
+		Map<Integer, Material> list = new TreeMap<Integer, Material>();
+		List<Material> resultList=super.findEntities(col,value);
+		for(Material m:resultList)
 		{
-			Query q=em.createQuery("Select x from Material x where x."+col+"='"+value+"'");
-			list = new HashMap<Integer, Material>();
-			List<Material> resultList=q.getResultList();
-			for(Material m:resultList)
-			{
-				list.put(m.getId(),m);
-			}
-		}
-		catch(PersistenceException pe)
-		{
-			em.getTransaction().rollback();
-			logger.error("DAO<"+getEntityClass().getSimpleName()+">:Error has occurred when trying to find entities");
-			throw new DAOException(pe);
+			list.put(m.getId(),m);
 		}
 		return list;
 	}
 
 	public Map<Integer, Material> findMaterials() throws DAOException
 	{
-		EntityManager em=getEntityManager();
-		Map<Integer, Material> list;
-		try
+		List<Material> resultList=super.findEntities();
+		Map<Integer, Material> list = new TreeMap<Integer, Material>();
+		for(Material m:resultList)
 		{
-			Query q=em.createQuery("Select x from Material x ORDER BY x.id");
-			list = new TreeMap<Integer, Material>();
-			List<Material> resultList=q.getResultList();
-			for(Material m:resultList)
-			{
-				list.put(m.getId(),m);
-			}
-		}
-		catch(PersistenceException pe)
-		{
-			em.getTransaction().rollback();
-			logger.error("DAO<"+getEntityClass().getSimpleName()+">:Error has occurred when trying to find entities");
-			throw new DAOException(pe);
+			list.put(m.getId(),m);
 		}
 		return list;
 	}
