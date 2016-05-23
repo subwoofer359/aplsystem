e991f9c6262820cccf867c5730923f64422667cc
Adrian McLaughlin <subwoofer359@gmail.com>
2016-05-23 18:02:46 +0100

    AS-19 implemented search request in PartsController

--- src/main/java/org/amc/servlet/PartsController.groovy
+++ src/main/java/org/amc/servlet/PartsController.groovy
@@ -1,22 +1,95 @@
 package org.amc.servlet
 
+import org.amc.servlet.action.SearchPartAction;
+import org.amc.servlet.action.search.PartSearch;
+import org.amc.servlet.model.PartSearchForm;
+import org.amc.servlet.validator.PartSearchFormValidator;
+import org.amc.servlet.validator.PartSearchFormValidator.PartSearchBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.text.ParseException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Spring Controller for handling Part operations and returning a view
  * @author Adrian Mclaughlin
  * @version 2
  */
+@SessionAttributes("PARTSEARCH")
 class PartsController {
 
-    @RequestMapping(method=RequestMethod.GET, value="APLSystemServlet")
+    static final String SEARCH = 'search';
+    static final String SESSION_PARTSEARCH = 'PARTSEARCH';
+    static final String COMPANY = 'company';
+    static final String PARTNAME = 'partName';
+    static final String QSS_NUMBER = 'qssNumber';
+    static final String MODEL_ATTR_PARTS = 'parts';
+    static final String VIEW_SEARCH_PAGE = 'PartsSearchPage'
+    static final String SEARCH_PARSE_ERROR_MSG = 'Search Parameters couldn\'t be parsed';
+
+    def partActionFactory;
+    
+    PartSearchFormValidator searchFormValidator;
+    
+    def partFormParser = PartSearchBinder.&getPartSearch;
+    
+    @RequestMapping(method = RequestMethod.GET, value = "APLSystemServlet")
     String getAPLSystemServlet() {
         return "Main";
     }
     
-    void logout() {
+    @RequestMapping(method = RequestMethod.GET, value = "logout")
+    String logout(HttpSession session, HttpServletRequest httpServletRequest) {
+        session?.invalidate();
+        httpServletRequest.logout();
         
+        return "redirect:/";
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "Part_display")
+    String displayPart() {
+        return 'Part';
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, value = "Part_search", params="mode=search")
+    ModelAndView searchForPart(HttpServletRequest request, @ModelAttribute('PARTSEARCH') final PartSearch lastPartSearch,
+        @RequestParam def mode, @RequestParam def edit) {
+        
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName(VIEW_SEARCH_PAGE);
+        
+        SearchPartAction spa = partActionFactory.getSearchJobTemplateAction();
+        List parts = Collections.EMPTY_LIST;
+        
+        def partSearchForm = new PartSearchForm();
+        partSearchForm.setCompany(request.getParameter(COMPANY));
+        partSearchForm.setPartName(request.getParameter(PARTNAME));
+        partSearchForm.setQSSNumber(request.getParameter(QSS_NUMBER));
+        
+        searchFormValidator.validate(partSearchForm);
+        
+        if (searchFormValidator.hasErrors()) {
+            mav.getModel().put(ControllerConstants.MESSAGE, searchFormValidator.getErrors());
+        } else if (partSearchForm.isEmpty()) {
+            parts = lastPartSearch ? spa.search(lastPartSearch) : [];
+        } else {
+           try {
+               def partSearch = partFormParser(partSearchForm);
+               parts = spa.search(partSearch);
+               mav.getModel().put(SESSION_PARTSEARCH, partSearch);
+           } catch(ParseException pe) {
+               mav.getModel().put(ControllerConstants.MESSAGE, SEARCH_PARSE_ERROR_MSG);
+           }
+           
+        }
+        mav.getModel().put(MODEL_ATTR_PARTS, parts);
+        return mav;
     }
 }
--- src/test/java/org/amc/servlet/PartsControllerTest.groovy
+++ src/test/java/org/amc/servlet/PartsControllerTest.groovy
@@ -1,18 +1,67 @@
 package org.amc.servlet;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import org.amc.dao.DAO;
+import org.amc.model.Part;
+import org.amc.servlet.action.PartActionFactory;
+import org.amc.servlet.action.SearchPartAction;
+import org.amc.servlet.action.search.PartSearch;
+import org.amc.servlet.validator.PartSearchFormValidator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.*;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.ParseException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 class PartsControllerTest {
     PartsController controller;
+    def mode = 'search';
+    def edit = '3';
+    def company = 'ALPS';
+    def partName = '2304 LightPipe';
+    def qssNumber = 'ALPS 234';
+    
+    @Mock
+    HttpSession session;
+    
+    @Mock
+    HttpServletRequest httpRequest;
+    
+    @Mock
+    PartActionFactory partActionFactory;
+    
+    @Mock
+    DAO<Part> jobDAO;
+    
+    @Mock
+    SearchPartAction searchPartAction;
+    
+    @Mock
+    PartSearchFormValidator searchFormValidator;
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        when(partActionFactory.getSearchJobTemplateAction()).thenReturn(searchPartAction);
         controller = new PartsController();
+        controller.partActionFactory = partActionFactory;
+        controller.searchFormValidator = searchFormValidator;
+        
+        when(httpRequest.getParameter('company')).thenReturn(company);
+        when(httpRequest.getParameter('partName')).thenReturn(partName);
+        when(httpRequest.getParameter('QSSNumber')).thenReturn(qssNumber);
+        
+        when(searchFormValidator.hasErrors()).thenReturn(false);
+        
     }
 
     @After
@@ -27,7 +76,95 @@ class PartsControllerTest {
     
     @Test
     public void testLogOut() {
-        controller.logout();
+        def redirect = controller.logout(session, httpRequest);
+        verify(session, times(1)).invalidate();
+        verify(httpRequest, times(1)).logout();
+        
+        assert redirect == "redirect:/";
+        
+    }
+    
+    @Test
+    public void testLogOutNoSession() {
+        controller.logout(null, httpRequest);
+        verify(session, times(0)).invalidate();
+    }
+    
+    @Test
+    public void testDisplayPart() {
+        String view = controller.displayPart();
+        assert view == "Part";   
+    }
+    
+    @Test
+    public void testSearchForPartSearchMode() {       
+        def parts = [new Part(partName, '3432', company, '3', '2', 'BLACK', true, qssNumber)];
+        when(searchPartAction.search(any())).thenReturn(parts);
+        
+        ModelAndView mav = controller.searchForPart(httpRequest, null, mode, edit);
+        ModelAndViewAssert.assertViewName(mav, "PartsSearchPage");
+        ModelAndViewAssert.assertModelAttributeAvailable(mav, "parts");
+        ModelAndViewAssert.assertModelAttributeValue(mav, "parts", parts);
+        
+        
+    }
+    
+    @Test
+    public void testSearchForPartBlankSearchNoSessionAttribute() {
+        def parts = [new Part(partName, '3432', company, '3', '2', 'BLACK', true, qssNumber)];
+        when(searchPartAction.search(any())).thenReturn(parts);
+        reset(httpRequest);
+        
+        ModelAndView mav = controller.searchForPart(httpRequest, null, mode, edit);
+        ModelAndViewAssert.assertViewName(mav, "PartsSearchPage");
+        ModelAndViewAssert.assertModelAttributeAvailable(mav, "parts");
+        ModelAndViewAssert.assertModelAttributeValue(mav, "parts", []);  
+    }
+    
+    @Test
+    public void testSearchForPartBlankSearchWithSessionAttribute() {
+        def parts = [new Part(partName, '3432', company, '3', '2', 'BLACK', true, qssNumber)];
+        when(searchPartAction.search(any())).thenReturn(parts);
+        reset(httpRequest);
+        
+        PartSearch lastSearch = new PartSearch();
+        lastSearch.company = company;
+        lastSearch.partName = partName;
+        lastSearch.QSSNumber = qssNumber;
+        
+        ModelAndView mav = controller.searchForPart(httpRequest, lastSearch, mode, edit);
+        ModelAndViewAssert.assertViewName(mav, "PartsSearchPage");
+        ModelAndViewAssert.assertModelAttributeAvailable(mav, "parts");
+        ModelAndViewAssert.assertModelAttributeValue(mav, "parts", parts);
+    }
+    
+    @Test
+    public void testNotValidSubmission() {
+        def parts = [];
+        when(searchPartAction.search(any())).thenReturn(parts);
+        reset(searchFormValidator);
+        when(searchFormValidator.hasErrors()).thenReturn(true);
+        when(searchFormValidator.getErrors()).thenReturn([]);
+        
+        ModelAndView mav = controller.searchForPart(httpRequest, null, mode, edit);
+        ModelAndViewAssert.assertViewName(mav, "PartsSearchPage");
+        ModelAndViewAssert.assertModelAttributeAvailable(mav, "parts");
+        ModelAndViewAssert.assertModelAttributeValue(mav, "parts", parts);
+        ModelAndViewAssert.assertAndReturnModelAttributeOfType(mav, ControllerConstants.MESSAGE, List); 
+    }
+    
+    @Test
+    public void testSearchParseException() {
+        def parts = [];
+        when(searchPartAction.search(any())).thenReturn(parts);
+        def newParser = { throw new ParseException('Test Parse Error', 1)};
+        controller.partFormParser = newParser;
+        
+        ModelAndView mav = controller.searchForPart(httpRequest, null, mode, edit);
+        ModelAndViewAssert.assertViewName(mav, "PartsSearchPage");
+        ModelAndViewAssert.assertModelAttributeAvailable(mav, "parts");
+        ModelAndViewAssert.assertModelAttributeValue(mav, "parts", parts);
+        ModelAndViewAssert.assertModelAttributeValue(mav, ControllerConstants.MESSAGE, PartsController.SEARCH_PARSE_ERROR_MSG);
     }
 
 }
