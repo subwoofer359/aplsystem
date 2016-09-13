2e599af46242a273c2ef78b9dad9d35538d6c8f0
Adrian McLaughlin <subwoofer359@gmail.com>
2016-09-13 13:03:07 +0100

    fix formatting issues with TablesSort.js

--- src/main/webapp/js/TablesSort.js
+++ src/main/webapp/js/TablesSort.js
@@ -10,32 +10,26 @@
  * @param {Object} table HTML table element
  * @param {string||number} columnToSortBy the column of the table to be sorted 
  */
-function tableSort(table,columnToSortBy)
-{
+function tableSort(table, columnToSortBy) {
 	//Presuming the table header element in a thead element
-	if(table.tagName.toLowerCase()!='table')
-	{
-		while(table.tagName.toLowerCase()!='table' && table.parentNode!=null)
+	if (table.tagName.toLowerCase() !== 'table')	{
+		while (table.tagName.toLowerCase() !== 'table' && table.parentNode != null)
 		{
-			table=table.parentNode;
+			table = table.parentNode;
 		}
 	}
+	
 	//Check to see if columnToSortBy is a string and set columnToSortBy to the corresponding column index
-	if(typeof columnToSortBy == 'string')
-	{
-		var thead;
-		if(table.tHead)
-		{
-			thead=table.tHead.rows;
-			if(thead.length>0)
-			{
-				var row=thead[0];
-				var i;
-				var rlen=row.cells.length;
-				for(i=0;i<rlen;i++)
-				{
-					if(row.cells[i].innerHTML.indexOf(columnToSortBy)!=-1)
-					{
+	if (typeof columnToSortBy === 'string') {
+		var thead,
+		    row;
+		if (table.tHead) {
+			thead = table.tHead.rows;
+			if (thead.length > 0) {
+				row = thead[0];
+						
+				for (var i = 0, rlen = row.cells.length; i < rlen; i++) {
+					if (row.cells[i].innerHTML.indexOf(columnToSortBy) !== -1) {
 						columnToSortBy=i;	
 						break;
 					}
@@ -43,59 +37,51 @@ function tableSort(table,columnToSortBy)
 			}
 		}
 	}
-	var tbody=table.tBodies;
+	
+	var tbody = table.tBodies;
 	//Check that there is at least one tbody defined
-	if(tbody.length<1)
-	{
+	if (tbody.length < 1) {
 		return;
 	}
-	else
-	{
-		tbody=tbody[0];
+	else {
+		tbody = tbody[0];
 	}
 
-	var rowLen=tbody.rows.length;
+	var rowLen = tbody.rows.length;
 	//Check the number of row in the table if the table is empty just return to caller
-	if(rowLen<1)
+	if (rowLen < 1)
 	{
 		return;
 	}
 	
-	var rows=tbody.rows;
+	var rows = tbody.rows;
 	//mxn array
-	var arr=[];
-	for(var i=0;i<rowLen;i++)
+	var arr = [];
+	for (var i = 0; i < rowLen; i++)
 	{
-		var cells=rows[i].cells;
-		arr[i]=[];
-		for(var j=0,cellLength=cells.length;j<cellLength;j++)
+		var cells = rows[i].cells;
+		arr[i] = [];
+		for (var j=0,cellLength = cells.length; j < cellLength; j++)
 		{
 	
-			arr[i][j]=cells[j].innerHTML;
+			arr[i][j] = cells[j].innerHTML;
 		}
 	}
-	console.log(arr);
 
-
-	arr.sort(
-	function(a,b)
-	{
-		if(a[columnToSortBy]<b[columnToSortBy])
-			return -1;
-		else if(a[columnToSortBy]>b[columnToSortBy])
+	arr.sort(function (a, b) {
+		if(a[columnToSortBy]<b[columnToSortBy]) {
+		    return -1;
+		} else if(a[columnToSortBy]>b[columnToSortBy]) {
 			return 1;
-		else
+		} else {
 			return 0;
-	}
-	);
+		}
+	});
 
-	for (var i = 0; i < rowLen; i++) 
-	{
-                //rows[i].innerHTML = "<td>" + arr[i].join("</td><td>") + "</td>";
-				for(var t=0;t < rlen;t++)
-				{
-					cell=rows[i].cells[t];
-					cell.innerHTML=arr[i][t];
-				}
+	for (var i = 0; i < rowLen; i++) {
+	    for(var t=0;t < rlen;t++){
+	        cell = rows[i].cells[t];
+			cell.innerHTML = arr[i][t];
+	    }
      }
 }
