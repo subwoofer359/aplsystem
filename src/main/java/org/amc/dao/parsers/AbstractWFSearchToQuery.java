a535e2162d8704fab2e53184007c465ab334d541
Adrian McLaughlin <subwoofer359@gmail.com>
2016-09-13 07:10:05 +0100

    add comments to AbstractWFSearchToQuery#QueryBuilder

--- src/main/java/org/amc/dao/parsers/AbstractWFSearchToQuery.java
+++ src/main/java/org/amc/dao/parsers/AbstractWFSearchToQuery.java
@@ -12,6 +12,7 @@ import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.Metamodel;
 
 public abstract class AbstractWFSearchToQuery implements WebFormSearchToQuery {
     
@@ -25,6 +26,12 @@ public abstract class AbstractWFSearchToQuery implements WebFormSearchToQuery {
         return cb;
     }
     
+    /**
+     * A Builder that helps to create a Criteria Query without a {@link Metamodel}
+     * 
+     * @author Adrian Mclaughlin
+     *
+     */
     static class QueryBuilder {
         private Root<? extends WorkEntity> root;
         private List<Predicate> predicates;
@@ -36,24 +43,47 @@ public abstract class AbstractWFSearchToQuery implements WebFormSearchToQuery {
             predicates = new ArrayList<Predicate>();
         }
         
+        /**
+         * Create a JPA like expression and adds it a List
+         * @param value String value to use
+         * @param fields String array relating to JPA model field name
+         */
         public void likeStringField(String value, String... fields) {
             if(value != null) {
                 predicates.add(cb.like(getExprStr(root, fields), value));
             }
         }
         
+        /**
+         * Create a JPA equal expression and adds it a {@link List}
+         * @param value String value to use
+         * @param fields String array relating to JPA model field name
+         */
         public void equalStringField(String value, String... fields) {
             if(value != null) {
                 predicates.add(cb.equal(getExprStr(root, fields), value));
             }
         }
         
+        /**
+         * Create a JPA equal expression and adds it a {@link List}
+         * @param value Object value to use
+         * @param clazz Class type to cast the expression to
+         * @param fields String array relating to JPA model field name
+         */
         public void equalField(Object value, Class<?> clazz, String... fields) {
             if(value != null) {
                 predicates.add(cb.equal(getExpr(root, clazz, fields), value));
             }
         }
         
+        /**
+         * Create a JPA between expression for Dates and adds it a {@link List}
+         * 
+         * @param startDate Date lower bound
+         * @param endDate Date upper bound
+         * @param fields String array JPA model date field name
+         */
         public void betweenDates(Date startDate, Date endDate, String... fields) {
             predicates.add(cb.between(getExprDate(root, fields), startDate, endDate));
             
@@ -63,6 +93,10 @@ public abstract class AbstractWFSearchToQuery implements WebFormSearchToQuery {
             return this.predicates;
         }
         
+        /**
+         * gets an array of Predicates
+         * @return a Predicate array
+         */
         public Predicate[] getArrayOfPredicates() {
             return predicates.toArray(new Predicate[predicates.size()]);
         }
